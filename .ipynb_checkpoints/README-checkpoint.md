009199be5f5f380cfdbe2c085ecd0ac93e5ea169
karlbue <kcb7zk@virginia.edu>
2024-01-28 13:29:41 -0500

    finished part1

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,27 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/Q_lTrP7n)
+# P5640ex00
+See the full description of this exercise in the assigned reading and on the course web site.
+
+
+## C++ starter code
+
+The starter code here (projGSL.cpp) demonstrates very basic usage of the gsl for solving a problem of coupled differential equations.  An 8th order R-K solver with fixed step size is used.  You are encouraged to try other solvers as you explore the problem.  See here for the gsl docs: https://www.gnu.org/software/gsl/doc/html/ode-initval.html
+
+This example solves the 2D projectile motion problem with a simple model for air resistance.  After each step, data are stored in ROOT TGraphs, which are then displayed at the conclusion of the calculation.  These are the basic pieces you will need to address the Duffing Oscillator problem.
+
+----
+## Python starter code
+
+Two examples are given for using ODE solvers from the scipy.integrate sub-package in Python.  In these examples graphs are made using matplotlib.
+
+1) Solution (projScPY2.py[ipynb]) using a more modern interface [scipy.integrate.solve_ivp](https://docs.scipy.org/doc/scipy/reference/reference/generated/scipy.integrate.solve_ivp.html#scipy.integrate.solve_ivp).  See also: https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html  and https://www.programcreek.com/python/example/119375/scipy.integrate.solve_ivp
+
+2) Solution (projScPY.py[ipynb]) using an older interface [scipy.integrate.odeint¶](https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html) (see comments here: https://docs.scipy.org/doc/scipy/reference/integrate.html).
+
+The notebook versions contain additional comments on using the integrators.
+
+----
+
+Give the computing IDs for team members here:  drb5wqd, 
+
+Push your solution code and PDF reports to this repo.
---
+++ run1.py
@@ -0,0 +1,107 @@
+from scipy.integrate import solve_ivp
+import numpy as np
+from math import sqrt
+
+period = 2 * np.pi / 2.4
+
+xlist = []
+plist = []
+
+def  func(t, y):
+    m = 1
+    a = 0.5
+    b = 0.25
+    F0 = 2
+    w = 2.4
+    gama = 0.1
+    x = y[0]
+    v = y[1]
+    f0 = y[1]
+    f1 = -(gama/m)*v + 2*(a/m)*x - 4*(b/m)*x*x*x + (F0/m)*np.cos(w*t)
+    return [f0,f1]
+
+y0 = [0.5, 0]
+
+
+#print("period" + str(period))
+t = np.linspace(0,period,200)
+#print(t)
+
+count = 25000
+
+for i in range(count):
+    sol = solve_ivp(func, [0,period], y0, t_eval=t)
+    yf = sol.y
+    #print(yf)
+    #for y in sol:
+    #    print(y)
+    print("x final: " + str(yf[0][-1]))
+    print("p final: " + str(yf[1][-1]))
+    xlist.append(yf[0][-1])
+    plist.append(yf[1][-1])
+    y0[0] = yf[0][-1]
+    y0[1] = yf[1][-1]
+
+l = 2
+matricies = []
+nums = []
+bs = []
+while l <= 128:
+    fractal = [[False for _ in range(l)] for _ in range(l)]
+    b = 6./float(l)
+    print("b = " + str(b))
+    for i in range(count):
+        div1 = xlist[i]/b
+        div2 = plist[i]/b
+        if div1 < 0:
+            div1-=1
+        if div2 < 0:
+            div2-=1
+        xpos = int(div1) + int(l/2)
+        ypos = int(div2) + int(l/2)
+        #print("xpos = " + str(xpos))
+        #print("ypos = " + str(ypos))
+        fractal[xpos][ypos] = True
+        
+    fraccount = 0
+    for i in range(l):
+        for j in range(l):
+            if fractal[i][j]:
+                fraccount += 1
+
+    print("N(b) = " + str(fraccount))
+    nums.append(fraccount)
+    bs.append(b)
+    matricies.append(fractal)
+    l *= 2
+
+
+    
+from matplotlib import pyplot as plt
+x = np.array(xlist)
+y = np.array(plist)
+x2 = np.array(np.log(bs))
+y2 = np.array(np.log(nums))
+
+plt.figure()
+plt.scatter(x,y, s=1)
+plt.xlabel('x')
+plt.ylabel('p')
+plt.title('Strange Attractor')
+
+plt.figure()
+plt.scatter(x2,y2)
+plt.xlabel('log(b)')
+plt.ylabel('log(N(b))')
+plt.title('Dimension')
+m, b = np.polyfit(x2,y2,deg=1)
+xseq = np.linspace(-3, 2, 100)
+plt.plot(xseq, b + m*xseq, color='k', lw=2.5)
+print("slope: " + str(m))
+
+print("Close plot window to exit")
+for m in matricies:
+    plt.matshow(m)
+plt.show()
+
+
